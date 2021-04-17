76ce39928b9c0414cfca10e1498fa7c0d16222ec
Kazuma Hatta <tori31001@gmail.com>
2021-04-17 23:48:44 +0900

    Update cmake_vs2015_64.bat

--- cmake_vs2015_64.bat
+++ cmake_vs2015_64.bat
@@ -2,7 +2,7 @@
 for /F "usebackq" %%i in (`forfiles /P "%VCPKG_DIR%\downloads" /m cmake* /c "cmd /c if @isdir==TRUE echo @path"`) do set CMAKE_DIR=%%i
 @echo on
 
-set CMAKE="%CMAKE_DIR%\bin\cmake.exe"
+set CMAKE=cmake
 set BUILD_DIR="build_vs2015_64"
 
 if not exist %BUILD_DIR% mkdir %BUILD_DIR%
