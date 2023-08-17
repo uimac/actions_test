13f2bf478087d8dad86c8971f9be40c861c18ce1
Kazuma Hatta <tori31001@gmail.com>
2023-08-17 23:28:13 +0900

    Update cmake_vs2022_64.bat

--- cmake_vs2022_64.bat
+++ cmake_vs2022_64.bat
@@ -3,5 +3,5 @@ set BUILD_DIR="build_vs2022_64"
 
 if not exist %BUILD_DIR% mkdir %BUILD_DIR%
 pushd %BUILD_DIR%
-%CMAKE% -D CMAKE_INSTALL_PREFIX=%VCPKG_DIR%/installed/x64-windows -G "Visual Studio 17 2022" .. 
+%CMAKE% -D CMAKE_INSTALL_PREFIX=%VCPKG_DIR%/installed/x64-windows-release -G "Visual Studio 17 2022" .. 
 popd
